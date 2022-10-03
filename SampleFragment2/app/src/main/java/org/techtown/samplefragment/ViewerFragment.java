bf6df23306d16384f2fa1f99e02f480642f22ce7
shurieLV <ckuper1080@gmail.com>
2022-10-04 07:05:23 +0900

    project init

---
+++ SampleFragment2/app/src/main/java/org/techtown/samplefragment/ViewerFragment.java
@@ -0,0 +1,28 @@
+package org.techtown.samplefragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import org.techtown.samplefragment2.R;
+
+public class ViewerFragment extends Fragment {
+    ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_viewer, container, false);
+
+        imageView = rootView.findViewById(R.id.imageView);
+        return rootView;
+    }
+
+    public void setImage(int resId){
+        imageView.setImageResource(resId);
+    }
+}
